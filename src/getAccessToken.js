ca55137b336b76734d019912b1871df23f620b68
Korey Moore <kmoorefit5@gmail.com>
2023-07-17 10:44:13 -0500

    added functionality to retrieve petfinder api access token

---
+++ src/getAccessToken.js
@@ -0,0 +1,21 @@
+const cache = {};
+
+const getAccessToken = async (apiUrl, clientId, clientSecret) => {
+  if (cache[apiUrl]) {
+    return cache[apiUrl];
+  }
+
+  const response = await fetch(apiUrl, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+    body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
+    mode: 'cors',
+  });
+  console.log(response);
+  const data = await response.json();
+  const accessToken = data.access_token;
+  cache[apiUrl] = accessToken;
+  return accessToken;
+}
+
+export default getAccessToken;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from 'vite';
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  root: "src",
+// eslint-disable-next-line no-unused-vars
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        define: {
+            'process.env.CLIENT_ID': JSON.stringify(env.CLIENT_ID),
+            'process.env.CLIENT_SECRET': JSON.stringify(env.CLIENT_SECRET)
+        },
+        plugins: [react()],
+        root: "src",
+    };
 });
